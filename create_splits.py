ee4ed29be5c14caa6d021b45ad33b7d0d26ddae0
RuchitJathania <44512765+RuchitJathania@users.noreply.github.com>
2022-06-24 14:07:28 -0400

    Creates splits for training and eval data

--- create_splits.py
+++ create_splits.py
@@ -2,10 +2,11 @@ import argparse
 import glob
 import os
 import random
+import shutil
 
 import numpy as np
 
-from utils import get_module_logger
+# from utils import get_module_logger
 
 
 def split(source, destination):
@@ -18,16 +19,29 @@ def split(source, destination):
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
     # TODO: Implement function
+    file_list = glob.glob(source+"\*.tfrecord")
+    train_list = random.sample(file_list, 90)
+    eval_list = random.sample(file_list,10)
+    shutil.rmtree(destination+r"\train")
+    shutil.rmtree(destination+r"\eval")
+    shutil.rmtree(destination+r"\test")
+    os.mkdir(destination+r"\train")
+    os.mkdir(destination+r"\eval")
+    os.mkdir(destination+r"\test")
+    for filepath in train_list:
+        shutil.copy(filepath,destination+r"\train")
+    for filepath in eval_list:
+        shutil.copy(filepath,destination+r"\eval")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
-    parser.add_argument('--source', required=True,
-                        help='source data directory')
-    parser.add_argument('--destination', required=True,
-                        help='destination data directory')
+    parser.add_argument('--source', required=False,
+                        help='source data directory', default=r'C:\OnlineCourses\SelfDrivingCarEngineer\ObjectDetectioninUrbanEnvironment\data\all')
+    parser.add_argument('--destination', required=False,
+                        help='destination data directory',default=r'C:\OnlineCourses\SelfDrivingCarEngineer\ObjectDetectioninUrbanEnvironment\data\newdata')
     args = parser.parse_args()
 
-    logger = get_module_logger(__name__)
-    logger.info('Creating splits...')
+    # logger = get_module_logger(__name__)
+    # logger.info('Creating splits...')
     split(args.source, args.destination)
